114f97b067c2beb297ead8d0b1f00d8eecb223e2
bairong <marvinzh6019@gmail.com>
2019-03-07 19:20:08 +0900

    init

--- ivec-cyclegan-pytorch/src/discriminator.py
+++ ivec-cyclegan-pytorch/src/discriminator.py
@@ -17,16 +17,20 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True)
             ]
         # (B,128,100)
-        
-
-        # FCN classification layer
-        model += [
-            nn.Conv1d(128, 512, kernel_size=100,padding=1)
-            ]
 
         self.model = nn.Sequential(*model)
+        self.fc1=nn.Linear(12800,512)
+        self.fc2=nn.Linear(512,512)
+        self.out = nn.Linear(512,1)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
 
     def forward(self, x):
         x =  self.model(x)
+        x = x.view(x.shape[0], -1)
+        a1 = self.leaky_relu(self.fc1(x))
+        a2 = self.leaky_relu(self.fc2(a1))
+        return self.out(a2)
+        
         # Average pooling and flatten
-        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
+        # return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
